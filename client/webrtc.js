8332d19ee5566e730d6b8b358ac2492f04897376
Piotr Szczygieł <szczygiel.p@gmail.com>
2021-05-26 16:51:55 +0200

    Working refactored version

---
+++ client/webrtc.js
@@ -0,0 +1,98 @@
+const ICE_CONFIG = {
+  iceServers: [
+    {
+      url: "stun:stun.szczygiel.dev",
+    },
+    {
+      urls: "turn:turn.szczygiel.dev?transport=udp",
+      credential: "ftj491q",
+      username: "piotr",
+    },
+    {
+      urls: "turn:turn.szczygiel.dev?transport=tcp",
+      credential: "ftj491q",
+      username: "piotr",
+    },
+  ],
+};
+
+const RtcEvent = {
+  OFFER: 1,
+  ANSWER: 2,
+  CANDIDATE: 3,
+};
+
+const rtcPeer = new RTCPeerConnection(ICE_CONFIG);
+let rtcChannel = rtcPeer.createDataChannel("channel");
+
+const rtcServer = new WebSocket("ws://ws.szczygiel.dev/webrtc");
+rtcServer.onerror = (error) => console.log("Server error", error);
+
+rtcServer.onmessage = (message) => {
+  console.debug("Got message", message.data);
+  const [event, data] = JSON.parse(message.data);
+  switch (event) {
+    case RtcEvent.OFFER:
+      rtcHandleOffer(data);
+      break;
+    case RtcEvent.ANSWER:
+      rtcHandleAnswer(data);
+      break;
+    case RtcEvent.CANDIDATE:
+      rtcHandleCandidate(data);
+      break;
+    default:
+      console.error("Unknown event", event);
+      break;
+  }
+};
+
+rtcServer.onopen = () => {
+  console.log("Connected to the signaling server");
+
+  rtcPeer.onicecandidate = (event) => {
+    if (event.candidate) {
+      rtcServer.send(JSON.stringify([RtcEvent.CANDIDATE, event.candidate]));
+    }
+  };
+
+  rtcChannel.onerror = (error) => console.log("Error on data channel", error);
+  rtcChannel.onclose = () => console.log("Data channel is closed");
+  rtcChannel.onmessage = (event) => handleMessage(event.data);
+  rtcPeer.ondatachannel = (event) => (rtcChannel = event.channel);
+};
+
+function rtcHandleOffer(offer) {
+  rtcPeer.setRemoteDescription(new RTCSessionDescription(offer));
+
+  rtcPeer.createAnswer(
+    (answer) => {
+      rtcPeer.setLocalDescription(answer);
+      rtcServer.send(JSON.stringify([RtcEvent.ANSWER, answer]));
+    },
+    (error) => console.error("Error creating an answer", error)
+  );
+}
+
+function rtcHandleCandidate(candidate) {
+  rtcPeer.addIceCandidate(new RTCIceCandidate(candidate));
+}
+
+function rtcHandleAnswer(answer) {
+  rtcPeer.setRemoteDescription(new RTCSessionDescription(answer));
+  console.log("Connection established successfully!!");
+}
+
+function rtcCreateOffer() {
+  rtcPeer.createOffer(
+    (offer) => {
+      rtcServer.send(JSON.stringify([RtcEvent.OFFER, offer]));
+      rtcPeer.setLocalDescription(offer);
+    },
+    (error) => console.error("Error creating an offer", error)
+  );
+}
+
+function rtcChannelSend(data) {
+  rtcChannel.send(data);
+}
--- server/server.js
+++ server/server.js
@@ -1,22 +1,34 @@
-const WebSocketServer = require("ws").Server;
-const wss = new WebSocketServer({ port: 8888 });
+const http = require("http");
+const WebSocket = require("ws");
 
-let connections = [];
+const server = http.createServer();
+const wss1 = new WebSocket.Server({ noServer: true });
+const wss2 = new WebSocket.Server({ noServer: true });
 
-wss.on("listening", function () {
-  console.log("Server started");
-});
-
-wss.on("connection", (connection) => {
-  console.log("User connected", connection);
-  connections.push(connection);
-
-  connection.on("close", () => console.log("Disconnecting user", connection));
-
-  connection.on("message", (message) => {
-    console.log("Message from user", message);
-    connections.forEach((conn) => {
-      if (conn !== connection) conn.send(message);
+wss1.on("connection", (ws, req) => {
+  const addr = `${req.socket.remoteAddress}:${req.socket.remotePort}`;
+  console.log(`Client ${addr} connected`);
+  ws.on("close", () => console.log(`Client ${addr} disconnected`));
+  ws.on("message", (message) => {
+    console.debug(`Received ${message.length} bytes from ${addr}`);
+    wss1.clients.forEach((client) => {
+      if (client !== ws) client.send(message);
     });
   });
 });
+
+server.on("upgrade", function upgrade(request, socket, head) {
+  if (request.url === "/webrtc") {
+    wss1.handleUpgrade(request, socket, head, (ws) =>
+      wss1.emit("connection", ws, request)
+    );
+  } else if (request.url === "/websocket") {
+    wss2.handleUpgrade(request, socket, head, (ws) =>
+      wss2.emit("connection", ws, request)
+    );
+  } else {
+    socket.destroy();
+  }
+});
+
+server.listen(8888);
